b2eec10345e8ba4e9eb279cd905a5b26c6b5d729
cbott <cbott6@gmail.com>
2017-11-21 12:07:00 -0500

    help message and n flag

--- avgtime.sh
+++ avgtime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OPTS=`getopt -o i:o: --long in:,out: -n 'parse-options' -- "$@"`
+OPTS=`getopt -o hi:o:n: --long help,in:,out:,number: -n 'parse-options' -- "$@"`
 
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 
@@ -7,11 +7,26 @@ eval set -- "$OPTS"
 
 infile=""
 outfile=""
+number=3
+
+show_help () {
+    echo "AvgTime"
+    echo "-------"
+    echo "Usage: avgtime.sh [-i <input file>] [-o <output file>] [-n <number of runs>] <command to time>"
+    echo "Flag | Long Name | Description"
+    echo " -h  | --help    | Show help message"
+    echo " -i  | --input   | Filename piped to command with < on each run"
+    echo " -o  | --output  | Filename to write output to with > on each run"
+    echo " -n  | --number  | Number of runs to average times across, default: 3"
+    exit 0
+}
 
 while true; do
   case "$1" in
+    -h | --help ) show_help; shift; shift ;;
     -i | --in ) infile="$2"; shift; shift ;;
     -o | --out ) outfile="$2"; shift; shift ;;
+    -n | --number ) number=$2; shift; shift ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -26,9 +41,9 @@ if [[ $outfile == "" ]]; then
 fi
 
 tempf=$(mktemp "${TMPDIR:-/tmp/}$(basename $0).XXXXXXXXXXXX")
-for x in {1..5}
+for x in $(seq 1 $number)
 do
-     /usr/bin/time -f "%U" -a -o $tempf $@ < $infile > $outfile
+    /usr/bin/time -f "%U" -a -o $tempf $@ < $infile > $outfile
 done
 awk '{ usertime += $1; count++ } END {  printf("Average User Time: %.3f seconds\n", usertime/count) }' $tempf >&2
 rm -f $tempf
